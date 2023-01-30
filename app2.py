eee4001966c42404a9aa50b877a983b5b6eb6cac
Ostara <71931135+edurrada@users.noreply.github.com>
2023-01-30 12:35:00 -0400

    Add files via upload
    Corrigido geração de imagens salvas ao longo do código; salvando apenas a versão final.

--- app2.py
+++ app2.py
@@ -2,31 +2,28 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-
 def cortar(img, w, h):
     width, height = img.size
-    newWidth = (height/(h/100))*(w/100) #dimensao final - 7:5
+    newWidth = (height/(h/100))*(w/100)
     box = ((width-newWidth)/2, 0, (width+newWidth)/2, height)
     img = img.crop(box)
     img = img.resize((w,h))
-    img.save(f'imagem{i}.jpg')
     return img
 
 def concatenar(img1, img2):
-    saida.paste(img1, (0, 0))
-    saida.paste(img2, (500, 0))
-    saida.save('saida.jpg')
+    result = Image.new("RGB", (2*width, height), "white")
+    result.paste(img1, (0, 0))
+    result.paste(img2, (500, 0))
+    return result
 
-def texto(frase1,frase2):
-    img = Image.open('saida.jpg')
+def texto(img, frase1, frase2):
     draw = ImageDraw.Draw(img)
     draw.text((150,28), frase1, font=fonte)
     draw.text((600,28), frase2, font=fonte)
-    img.save('saida2.jpg')
+    return img
 
-
-imagem1= Image.open('laptop.jpg')
-imagem2= Image.open('paisagem.jpg')
+imagem1 = Image.open('laptop.jpg')
+imagem2 = Image.open('paisagem.jpg')
 
 width, height = 500, 700
 
@@ -35,10 +32,9 @@ frase2 = '...Into that!!!'
 
 fonte = ImageFont.truetype("arial.ttf", 50)
 
-Imagens = [imagem1, imagem2]
-for i, imagem in enumerate (Imagens):
-    Imagens[i]=cortar(imagem, width, height)
-saida = Image.new("RGB", (2*width, height), "white")
-concatenar(Image.open('imagem0.jpg'),Image.open('imagem1.jpg'))
-texto(frase1,frase2)
+img1 = cortar(imagem1, width, height)
+img2 = cortar(imagem2, width, height)
+result = concatenar(img1, img2)
+result = texto(result, frase1, frase2)
 
+result.save('result.jpg')
